6b4b7dee8109926bc90b285d17bc1c7ab5920520
raphael <raphaelg2012@gmail.com>
2020-04-23 20:32:02 -0700

    setup folder structure

--- src/components/main/App.tsx
+++ src/components/main/App.tsx
@@ -13,7 +13,7 @@ class App extends React.Component {
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React itotess
+            Learn React waaa
           </a>
         </header>
       </div>
