5ceca97474d9207a3338eb3d3c97247105f172db
raphael <raphaelg2012@gmail.com>
2020-04-29 21:54:17 -0700

    setup router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,13 +2,16 @@
 import React, { Component } from 'react';
 import AppStyles from './App.module.scss';
 import SignupLoginContainer from '../SignupLoginContainer/SignupLogin';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className={AppStyles.App}>
-        <SignupLoginContainer />
-      </div>
+      <BrowserRouter>
+        <div className={AppStyles.App}>
+          <SignupLoginContainer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
