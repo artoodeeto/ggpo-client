cac7d34d966081385e6b0a055e964f3d2d85e97a
raphael <raphaelg2012@gmail.com>
2020-05-04 22:02:11 -0700

    moved main routing to app from signuplogin

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,16 +2,24 @@
 import React, { Component } from 'react';
 import AppStyles from './App.module.scss';
 import SignupLoginContainer from '../SignupLoginContainer/SignupLogin';
-import { BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import Feed from 'components/Feed/Feed';
+import Profile from 'components/Profile/Profile';
+import Private from 'components/Private/Private';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className={AppStyles.App}>
-          <SignupLoginContainer />
-        </div>
-      </BrowserRouter>
+      <div className={AppStyles.App}>
+        <Switch>
+          <Route path="/" exact>
+            <SignupLoginContainer />
+          </Route>
+          <Private path="/profile" component={Profile} />
+          <Private path="/feed" component={Feed} />
+          <Route path="/not-found" render={() => <h1>NOT FOUND</h1>}></Route>
+        </Switch>
+      </div>
     );
   }
 }
--- src/components/SignupLoginContainer/SignupLogin.tsx
+++ src/components/SignupLoginContainer/SignupLogin.tsx
@@ -1,37 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import Login from './Login/Login';
 import Signup from './Signup/Signup';
-import { Route, NavLink, Switch } from 'react-router-dom';
 
 // background here should show some video, maybe the TI championship games
 class SignupLoginContainer extends Component {
   render() {
     return (
       <Fragment>
-        <header>
-          <nav>
-            <ul>
-              <li>
-                <NavLink to="/">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/signup">Signup</NavLink>
-              </li>
-              <li>
-                <NavLink to="/login">Login</NavLink>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <div>
-          <Switch>
-            <Route path="/signup" exact>
-              <Signup />
-            </Route>
-            <Route path="/login" exact component={Login} />
-            <Route path="/" exact render={() => <h1>HOME</h1>} />
-          </Switch>
-        </div>
+        <Signup />
+        <Login />
       </Fragment>
     );
   }
