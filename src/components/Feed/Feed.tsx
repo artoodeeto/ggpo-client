ce8554d609de4d0ca13788ac33add576246796b6
raphael <raphaelg2012@gmail.com>
2020-05-05 21:44:25 -0700

    WIP: testing router redirect

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -2,14 +2,21 @@ import React, { useEffect } from 'react';
 import { State } from 'interfaces/stateInterface';
 import { connect } from 'react-redux';
 import * as userSelectors from '../../models/User/selectors';
+import { useHistory } from 'react-router-dom';
 
 const Feed = (props: any) => {
+  const history = useHistory();
+  const onTest = () => {
+    history.push('/profile');
+  };
+
   useEffect(() => {
     console.log(props.isAuthenticated);
   });
 
   return (
     <div>
+      <button onClick={onTest}>Profile</button>
       <h1>I am FEED TEST!</h1>
     </div>
   );
