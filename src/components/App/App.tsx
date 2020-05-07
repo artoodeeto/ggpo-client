f89a667d34178740d6d7821954747e903610f239
raphael <raphaelg2012@gmail.com>
2020-05-06 20:59:29 -0700

    refactor routes, added main route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,13 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import AppStyles from './App.module.scss';
-import SignupLoginContainer from '../SignupLoginContainer/SignupLogin';
-import { Route, Switch } from 'react-router-dom';
-import Feed from 'components/Feed/Feed';
-import Profile from 'components/Profile/Profile';
-import Private from 'components/Private/Private';
+import MainRoute from 'routes/main.router';
 
 class App extends Component {
   render() {
     return (
       <div className={AppStyles.App}>
-        <Switch>
-          <Route path="/" exact>
-            <SignupLoginContainer />
-          </Route>
-          <Private path="/profile" component={Profile} />
-          <Private path="/feed" component={Feed} />
-          <Route path="/not-found" render={() => <h1>NOT FOUND</h1>}></Route>
-        </Switch>
+        <MainRoute />
       </div>
     );
   }
