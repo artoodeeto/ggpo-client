6eb570d5bf44d3d39cae66b8ab3e4c89166562c3
artoodeeto <raphaelg2012@gmail.com>
2020-10-17 16:25:23 -0700

    refactor signup and login to use sigle form

--- src/components/SignupLogin/SignupLoginContainer.tsx
+++ src/components/SignupLogin/SignupLoginContainer.tsx
@@ -1,6 +1,4 @@
 import React, { Component, Fragment } from 'react';
-import Login from './Login/Login';
-import Signup from './Signup/Signup';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import ErrorMsg from 'components/shared/ErrorMessage/ErrorMsg';
@@ -8,16 +6,34 @@ import { RootState } from 'store/root/root_reducer';
 import { hasErrorOnSignupOrLogin, isUserAuthorized } from 'store/session/Selectors';
 import { AnyAction } from 'redux';
 import { ThunkDispatch } from 'redux-thunk';
+import { Signup } from './Signup/Signup';
+import { Login } from './Login/Login';
+import styles from './SignupLoginContainer.module.scss';
+import { LoginSignupSliderButton } from './LoginSignupSliderButton/LoginSignupSliderButton';
 
 interface SignupLoginContainerProps {
   hasErrors: boolean;
   isAuthenticated: boolean;
 }
 
-interface SignupLoginContainerState {}
+interface SignupLoginContainerState {
+  toLoginOrSignup: boolean;
+}
 
 // background here should show some video, maybe the TI championship games
+// TODO: sign up and login button should show loading GIF on the their buttons
 class SignupLoginContainer extends Component<SignupLoginContainerProps, SignupLoginContainerState> {
+  constructor(props: SignupLoginContainerProps) {
+    super(props);
+    this.state = {
+      toLoginOrSignup: true
+    };
+  }
+
+  toLogInOrSignUp(toShow: boolean): void {
+    this.setState({ toLoginOrSignup: toShow });
+  }
+
   render() {
     return (
       <Fragment>
@@ -25,17 +41,23 @@ class SignupLoginContainer extends Component<SignupLoginContainerProps, SignupLo
           <div>
             <ErrorMsg />
           </div>
-        ) : (
-          ''
-        )}
+        ) : null}
 
         {this.props.isAuthenticated ? (
           <Redirect to="/feed" />
         ) : (
-          <Fragment>
-            <Signup />
-            <Login />
-          </Fragment>
+          <div className={styles.SignupLoginContainer}>
+            <div className={styles.SignupLoginContainer__SliderBtnContainer}>
+              <LoginSignupSliderButton toShow={(val: boolean) => this.toLogInOrSignUp(val)} />
+            </div>
+            <div className={styles.SignupLoginContainer__Form_Container}>
+              {this.state.toLoginOrSignup ? (
+                <Login isLoggingInOrSigningUp={this.state.toLoginOrSignup} />
+              ) : (
+                <Signup isLoggingInOrSigningUp={this.state.toLoginOrSignup} />
+              )}
+            </div>
+          </div>
         )}
       </Fragment>
     );
